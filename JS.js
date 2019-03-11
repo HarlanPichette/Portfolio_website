cf9f33eb5e203f17959cc8b9bc668cd7bb0ec5c7
Harlan Pichette <h.s.pichette@gmail.com>
2019-03-11 15:13:46 +0800

    Javascript integration

---
+++ JS.js
@@ -0,0 +1,16 @@
+console.log("hellow world"); 
+//inlne comment
+/* multi line comment */
+
+
+var myName = 'big dog';
+//will work throughout the etire program
+let ourName = 'something';
+//will only work within scope that it was declared in
+const pi = 3.14;
+//variable that will never change
+
+
+
+
+

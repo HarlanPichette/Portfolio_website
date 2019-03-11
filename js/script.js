8e202de552fd1b15149c2d5359def9c4d9c71b54
Harlan Pichette <h.s.pichette@gmail.com>
2019-03-11 23:23:18 +0800

    addition of project page, js transitions needed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-function showDate(){
-    var time = document.getElementById('time');
-    time.innerHTML = Date();
-    }
-    
-    function clearDate(){
-    var time = document.getElementById('time');
-    time.innerHTML = 'nothing';
-    }
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function nextSlide(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySLides");
+  if (n > x.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  x[slideIndex-1].style.display = "block"; 
+}

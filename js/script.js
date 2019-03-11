bcc0b4540f62e70d34af7868bdacb5135b0b057b
Harlan Pichette <h.s.pichette@gmail.com>
2019-03-11 19:10:25 +0800

    Addition of project page

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
-console.log("hello world"); 
-//inlne comment
-/* multi line comment */
-
-var myName = 'big dog';
-//will work throughout the etire program
-let ourName = 'something';
-//will only work within scope that it was declared in
-const pi = 3.14;
-//variable that will never change
-
-
-
-
-document.body.innerHTML = <h1> myName </h1>
-
-
-
-
+function showDate(){
+    var time = document.getElementById('time');
+    time.innerHTML = Date();
+    }
+    
+    function clearDate(){
+    var time = document.getElementById('time');
+    time.innerHTML = 'nothing';
+    }

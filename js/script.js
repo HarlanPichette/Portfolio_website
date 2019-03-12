a26aafd22d43417e50a3dcf2e2f6058a5fe39a2d
Harlan Pichette <h.s.pichette@gmail.com>
2019-03-12 20:16:25 +0800

    slider

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+let sliderImages = document.querySelectorAll('.slide');
+let arrowLeft = document.querySelector('#arrow-left');
+let arrowRight = document.querySelector('#arrow-right');
+let current = 0;
 
-function nextSlide(n) {
-  showDivs(slideIndex += n);
+//loop trhough the images and set the display to none
+function reset(){
+    for(let i = 0; i < sliderImages.length; i++){
+    sliderImages[i].style.display = 'none';
+    }
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySLides");
-  if (n > x.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
-  x[slideIndex-1].style.display = "block"; 
+function startSlide(){  reset(); sliderImages[0].style.display = 'block'; }
+
+startSlide();
+
+arrowRight.addEventListener('click', function(){
+reset();
+if(current === sliderImages.length - 1) { 
+    current = 0; 
+    
+    sliderImages[current].style.display = 'block';
 }
+else{ current++; sliderImages[current].style.display = 'block';}
+});
+
+arrowLeft.addEventListener('click', function(){
+reset();
+
+if(current === 0) { current = sliderImages.length-1; sliderImages[current].style.display = 'block';}
+else{ current--; sliderImages[current].style.display = 'block';}
+});
